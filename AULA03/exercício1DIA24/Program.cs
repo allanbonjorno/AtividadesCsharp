db896e9c01fedfc57f62a217c2308621021b45f7
allanbonjorno <allanprofessional@outlook.com>
2023-08-24 15:32:32 -0300

    EXERCICIOS DIA 24 DE AGOSTO

---
+++ AULA03/exercício1DIA24/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace exercício1DIA24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Digite um número : ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("Número PAR.");
+            }
+            else
+            {
+                Console.WriteLine("Número impar.");
+            }
+        }
+
+    }
+}
